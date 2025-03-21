21eb44267aed4542452efa0c0daa8db48e030267
johanneclc <johannecalcoen@gmail.com>
2025-03-21 16:25:10 +0100

    create menu composant

---
+++ portfolio-frontend/app/components/Menu/menu.tsx
@@ -0,0 +1,11 @@
+import './menu.css';
+
+export default function Menu() {
+    return (
+        <ul className="menu">
+          <li><a href="/about">A propos</a></li>
+          <li><a href="/projects">Projets</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+    );
+}
