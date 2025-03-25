a4f4ac493fcdb24a00c324d304e2f5a4b768ab86
johanneclc <johannecalcoen@gmail.com>
2025-03-25 09:53:25 +0100

    use MenuHome into Menu

--- portfolio-frontend/app/components/Menu/menu.tsx
+++ portfolio-frontend/app/components/Menu/menu.tsx
@@ -1,11 +1,7 @@
+import MenuHome from "../MenuHome/menu-home";
 import "./menu.css";
 
 export default function Menu() { return (
-    <ul className="menu">
-      <li><a href="/about">A propos</a></li>
-      <li><a href="/projects">Projets</a></li>
-      <li><a href="/recommandations">Recommandations</a></li>
-      <li><a href="/contact">Contact</a></li>
-    </ul>
+    <MenuHome />
 );
 }
