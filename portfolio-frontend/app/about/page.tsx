36336e559ac1f18c82ccf676dba76f185067a2ee
johanneclc <johannecalcoen@gmail.com>
2025-03-25 16:21:25 +0100

    [feat] create about page

---
+++ portfolio-frontend/app/about/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import Banner from "../components/Banner/banner"
+import Menu from "../components/Menu/menu"
+import "./about.css"
+
+export default function About() {
+  return (
+    <div>
+      <Menu />
+      <Banner title="Qui suis-je ?" />
+    </div>
+  )
+}
