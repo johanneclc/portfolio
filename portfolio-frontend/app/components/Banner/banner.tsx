b18c44417e6c58067c1316513724c01613a4dbee
johanneclc <johannecalcoen@gmail.com>
2025-03-31 10:44:26 +0200

    [style] update css files

--- portfolio-frontend/app/components/Banner/banner.tsx
+++ portfolio-frontend/app/components/Banner/banner.tsx
@@ -3,13 +3,8 @@ import "./banner.css"
 export default function Banner({ title }: { title: string }) {
   return (
     <div>
-      <div
-        className="flex justify-center "
-        style={{ paddingTop: "85px", backgroundColor: "black" }}
-      >
-        <p className="page-title font-extrabold text-transparent bg-fixed">
-          {title}
-        </p>
+      <div className="flex justify-center" style={{ paddingTop: "85px" }}>
+        <p className="page-title font-extrabold">{title}</p>
       </div>
     </div>
   )
